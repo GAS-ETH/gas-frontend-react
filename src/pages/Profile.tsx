6d5d9b2f577f1ca259e36095406ec5ee7db93666
gylman <gylmanbm@gmail.com>
2023-06-03 03:22:50 +0900

    profile photo

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import styled from 'styled-components';
+import logo from '../assets/personal-center-human-shape.png';
 
-const Profile = () => {
-  return <div>Profile</div>;
-};
+const OutmostWrapper = styled.div`
+  display: flex;
+`;
 
+const LogoWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  border-radius: 50%;
+  background-color: red;
+  width: 36px;
+  height: 36px;
+`;
+
+const Logo = styled.img.attrs({
+  src: logo,
+  width: '32px',
+  height: '32px',
+})``;
+
+const Profile = () => (
+  <OutmostWrapper>
+    <LogoWrapper>
+      <Logo />
+    </LogoWrapper>
+  </OutmostWrapper>
+);
 export default Profile;
